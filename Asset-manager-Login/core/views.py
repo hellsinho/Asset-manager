f18212e010e977b9e6f1144edbe5bd30568d2dd2
Edson <edsonnunes360@gmail.com>
2023-08-01 04:38:28 -0300

    99%

---
+++ Asset-manager-Login/core/views.py
@@ -0,0 +1,38 @@
+# login/views.py
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            error_message = "Usuário ou senha incorretos."
+            return render(request, 'login.html', {'error_message': error_message})
+
+    return render(request, 'login.html')
+
+@login_required
+def dashboard_view(request):
+    return render(request, 'dashboard/dashboard.html')
+
+@login_required
+def ativos_view(request):
+    return render(request, 'dashboard/Ativos/dashboardAtivos.html')
+
+@login_required
+def ativosEstatisticas_view(request):
+    return render(request, 'dashboard/estatisticasAleatorias.html')
+
+@login_required
+def estatisticasAleatorias_view(request):
+    return render(request, 'dashboard/estatisticasAleatorias.html')
+
+@login_required
+def filtroseaeradores_view(request):
+    return render(request, 'dashboard/filtros.e.aeradores.html')
